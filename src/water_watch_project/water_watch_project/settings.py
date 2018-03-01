3e2639b6ae3e2a8bcd6676bbd31a17cf853490ba
Thy Vo <thyvo@thys-mbp.attlocal.net>
2018-03-01 14:33:40 -0800

    Update: Django Rest Framework version is 3.6.2

--- src/water_watch_project/water_watch_project/settings.py
+++ src/water_watch_project/water_watch_project/settings.py
@@ -73,12 +73,24 @@ WSGI_APPLICATION = 'water_watch_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'waterwatch',
+        'USER': 'waterwatchadmin',
+        'PASSWORD': 'khongcopw', #replace your own pw
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
