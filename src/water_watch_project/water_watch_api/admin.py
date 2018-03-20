6ce3f9e2ae1f19d89251d659ca8bc1b01ffad3f2
Emma Peatfield <emma.peatfield@sjsu.edu>
2018-03-19 19:37:20 -0700

    Added search bars to each page

--- src/water_watch_project/water_watch_api/admin.py
+++ src/water_watch_project/water_watch_api/admin.py
@@ -9,23 +9,27 @@ class StationAdmin(admin.ModelAdmin):
 
     list_filter = ['us_state_cd']
     list_display = ('id', 'station_name', 'us_state_cd', 'longitude', 'latitude')
+    search_fields = ('id', 'station_name', 'us_state_cd',)
 
 
 class SensorAdmin(admin.ModelAdmin):
 
     list_filter = ('current_status', 'station__station_name', 'sensor_type__sensor_type_name')
     list_display = ('id', 'station', 'sensor_type', 'current_status')
+    search_fields = ('station__station_name', 'sensor_type__sensor_type_name','current_status',)
 
 
 class SensorTypeAdmin(admin.ModelAdmin):
 
     list_display = ('id', 'sensor_type_name', 'description', 'unit')
+    search_fields = ('sensor_type_name',)
 
 
 class SensorDataAdmin(admin.ModelAdmin):
 
     list_display = ('id', 'sensor', 'sensor_data_dateTime', 'sensor_data_value')
     list_filter = ('sensor__station__station_name', 'sensor__sensor_type__sensor_type_name', ('sensor_data_dateTime', DateFieldListFilter),)
+    search_fields = ('sensor__station__station_name', 'sensor_data_value','sensor__sensor_type__sensor_type_name')
 
 
 admin.site.register(models.Station, StationAdmin)
