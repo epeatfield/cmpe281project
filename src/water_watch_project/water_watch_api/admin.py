66a694a7b6d912e7d9ac5590c3bb029a5f862f39
Emma Peatfield <emma.peatfield@sjsu.edu>
2018-03-20 20:27:56 -0700

    Added pahination

--- src/water_watch_project/water_watch_api/admin.py
+++ src/water_watch_project/water_watch_api/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.contrib.admin import DateFieldListFilter
+from django.contrib.admin import ModelAdmin
 
 from . import models
+
 # Register your models here.
 
 
@@ -10,27 +12,27 @@ class StationAdmin(admin.ModelAdmin):
     list_filter = ['us_state_cd']
     list_display = ('id', 'station_name', 'us_state_cd', 'longitude', 'latitude')
     search_fields = ('id', 'station_name', 'us_state_cd',)
-
+    list_per_page = 5
 
 class SensorAdmin(admin.ModelAdmin):
 
     list_filter = ('current_status', 'station__station_name', 'sensor_type__sensor_type_name')
     list_display = ('id', 'station', 'sensor_type', 'current_status')
     search_fields = ('station__station_name', 'sensor_type__sensor_type_name','current_status',)
-
+    list_per_page = 5
 
 class SensorTypeAdmin(admin.ModelAdmin):
 
     list_display = ('id', 'sensor_type_name', 'description', 'unit')
     search_fields = ('sensor_type_name',)
-
+    list_per_page = 5
 
 class SensorDataAdmin(admin.ModelAdmin):
 
     list_display = ('id', 'sensor', 'sensor_data_dateTime', 'sensor_data_value')
     list_filter = ('sensor__station__station_name', 'sensor__sensor_type__sensor_type_name', ('sensor_data_dateTime', DateFieldListFilter),)
     search_fields = ('sensor__station__station_name', 'sensor_data_value','sensor__sensor_type__sensor_type_name')
-
+    list_per_page = 10
 
 admin.site.register(models.Station, StationAdmin)
 admin.site.register(models.SensorType, SensorTypeAdmin)
--- src/water_watch_project/water_watch_api/models/sensor_data.py
+++ src/water_watch_project/water_watch_api/models/sensor_data.py
@@ -1,3 +1,4 @@
+
 from django.db import models
 
 from . import Sensor
@@ -11,6 +12,7 @@ class SensorData(models.Model):
     sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
     sensor_data_dateTime = models.DateTimeField()
     sensor_data_value = models.DecimalField(max_digits=20, decimal_places=3)
+
 
     def __str__(self):
         return str(self.sensor) + '-' + str(self.sensor_data_value) + '-' + str(self.sensor_data_dateTime)
