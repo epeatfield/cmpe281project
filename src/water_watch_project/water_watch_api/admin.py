b8e9e6da0f677658a051e43be3dd1f70a7938687
Emma Peatfield <emma.peatfield@sjsu.edu>
2018-03-22 10:27:04 -0700

    created an action to export sensor data as a csv file

--- src/water_watch_project/water_watch_api/admin.py
+++ src/water_watch_project/water_watch_api/admin.py
@@ -31,7 +31,27 @@ class SensorTypeAdmin(admin.ModelAdmin):
 
 class SensorDataAdmin(admin.ModelAdmin):
 
+    actions = ['download_csv']
     list_display = ('id', 'sensor', 'sensor_data_dateTime', 'sensor_data_value')
+    def download_csv(self, request, queryset):
+        #None
+        import csv
+        from django.http import HttpResponse
+        import StringIO
+
+        f = StringIO.StringIO()
+        writer = csv.writer(f)
+        writer.writerow(["id","sensor","sensor date","sensor value"])
+
+        for s in queryset:
+            writer.writerow([s.id, s.sensor, s.sensor_data_dateTime, s.sensor_data_value])
+
+        f.seek(0)
+        response = HttpResponse(f, content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=sensor-data.csv'
+        return response
+
+    download_csv.short_description = "Download CSV file for selected stats."
     list_filter = ('sensor__station__station_name', 'sensor__sensor_type__sensor_type_name', ('sensor_data_dateTime', DateTimeRangeFilter),)
     search_fields = ('sensor__station__station_name', 'sensor_data_value','sensor__sensor_type__sensor_type_name')
     list_per_page = 10
