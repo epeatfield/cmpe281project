8a2edbd5a13c93858f882ada1fcd70d9ccadb7a0
Thy Vo <hoangthy.vo@sjsu.edu>
2018-04-05 12:02:16 -0700

    Added EB database config and requirements files

--- src/water_watch_project/water_watch_project/settings.py
+++ src/water_watch_project/water_watch_project/settings.py
@@ -100,18 +100,28 @@ WSGI_APPLICATION = 'water_watch_project.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'waterwatch',
-        'USER': 'waterwatchadmin',
-        'PASSWORD': 'khongcopw', #replace your own pw
-        'HOST': 'localhost',
-        'PORT': '',
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
-
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'waterwatch',
+            'USER': 'waterwatchadmin',
+            'PASSWORD': 'khongcopw',  # replace your own pw
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
